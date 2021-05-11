b0d681f0f146470cb917a8a582b17dd439203117
Rintu Rajak <rintu.rajak@gmail.com>
2021-05-11 18:28:23 +0530

    initial version

---
+++ src/test/java/com/mycompany/bs/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.bs.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
